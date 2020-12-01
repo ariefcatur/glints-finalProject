ae4002280617651c272349e0f99ded5e584bd414
rifklidannyprakoso <rifklidanny@gmail.com>
2020-12-01 22:25:47 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import './App.css';
 import { Route, Switch, withRouter, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard'
 import History from './pages/History'
+import Profile from './components/Profile.js'
+import Footer from './components/Footer.js'
 
 
 function App(){
@@ -17,6 +19,9 @@ function App(){
             <li>
               <Link to="/History"> History </Link>
             </li>
+            <li> 
+              <Link to="/Profie"> Profile </Link>
+            </li>
             <li>
               <Link to="/Home"> Home </Link>
             </li>
@@ -28,25 +33,17 @@ function App(){
       <Route path ="/history">
         <History />
       </Route>
+      <Route path ="/profile">
+        <Profile />
+      </Route>
       <Route path="/">
             <Home />
       </Route>
     </Switch>
+    <Footer/>
     </>
   )
-=======
-import React from 'react'
-import Profile from './components/Profile.js'
-import Footer from './components/Footer.js'
-
-function App() {
-  return (
-    <div className="App">
-     <Profile/>
-     <Footer/>
-    </div>
-  );
->>>>>>> origin/profilePage
 }
+
 
 export default withRouter(App);
